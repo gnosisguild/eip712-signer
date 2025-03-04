5da25eabd617ca0b92f885b4e339d2682c0d0aed
Cristóvão <cris@gnosisguild.org>
2025-03-04 02:04:56 +0100

    SignTypedMessageLib fallback work

--- contracts/SignTypedMessageLib.sol
+++ contracts/SignTypedMessageLib.sol
@@ -2,19 +2,24 @@
 pragma solidity >=0.8.21;
 
 import "./SafeStorage.sol";
-import "./EIP712Hasher.sol";
+import "./EIP712Encoder.sol";
 
 interface ISafe {
   function domainSeparator() external view returns (bytes32);
 }
 
-contract SignTypedMessageLib is SafeStorage, EIP712Hasher {
+contract SignTypedMessageLib is SafeStorage, EIP712Encoder {
   address public immutable deployedAt;
+  bytes32 public immutable safeMessageTypeHash;
 
   event SignMsg(bytes32 indexed msgHash);
 
   constructor() {
     deployedAt = address(this);
+    //safeMessageTypeHash = keccak256("SafeMessage(bytes message)");
+    (safeMessageTypeHash) = (
+      0x60b3cbf8b4a223d68d641b3b6ddf9a298e7f33710cf3d3a9d1146b5a6150fbca
+    );
   }
 
   /**
@@ -23,9 +28,10 @@ contract SignTypedMessageLib is SafeStorage, EIP712Hasher {
    * @param message Arbitrary length data that should be marked as signed on the behalf of address(this).
    */
   function signMessage(bytes calldata message) external {
-    bytes32 msgHash = hashSafeMessage(message);
-    signedMessages[msgHash] = 1;
-    emit SignMsg(msgHash);
+    bytes32 safeMessageHash = hashSafeMessage(message);
+
+    signedMessages[safeMessageHash] = 1;
+    emit SignMsg(safeMessageHash);
   }
 
   /**
@@ -39,30 +45,24 @@ contract SignTypedMessageLib is SafeStorage, EIP712Hasher {
     bytes calldata message,
     AbiParam[] calldata types
   ) public {
-    require(address(this) != deployedAt, "Delegatecall Only!");
+    require(address(this) != deployedAt);
 
-    bytes32 msgHash = hashSafeMessage(
-      abi.encode(hashTypedMessage(domain, message, types))
+    bytes32 safeMessageHash = hashSafeMessage(
+      abi.encode(hashTypedData(domain, message, types))
     );
-    signedMessages[msgHash] = 1;
-    emit SignMsg(msgHash);
+
+    signedMessages[safeMessageHash] = 1;
+    emit SignMsg(safeMessageHash);
   }
 
-  // keccak256("SafeMessage(bytes message)");
-  bytes32 private constant SAFE_MSG_TYPEHASH =
-    0x60b3cbf8b4a223d68d641b3b6ddf9a298e7f33710cf3d3a9d1146b5a6150fbca;
-
   function hashSafeMessage(bytes memory message) public view returns (bytes32) {
-    bytes32 payloadHash = keccak256(
-      abi.encode(SAFE_MSG_TYPEHASH, keccak256(message))
-    );
     return
       keccak256(
         abi.encodePacked(
           bytes1(0x19),
           bytes1(0x01),
           ISafe(address(this)).domainSeparator(),
-          payloadHash
+          keccak256(abi.encode(safeMessageTypeHash, keccak256(message)))
         )
       );
   }
@@ -73,20 +73,24 @@ contract SignTypedMessageLib is SafeStorage, EIP712Hasher {
    * aliases, working around the overly strict integrity checks of the RolesMod
    */
   fallback() external {
+    uint256 temp;
     bytes calldata domain;
     bytes calldata message;
     AbiParam[] calldata types;
 
-    bytes calldata buffer = msg.data;
-
     assembly {
-      domain.offset := add(buffer.offset, 4)
-      domain.length := add(calldataload(domain.offset), 32)
-
-      message.offset := add(domain.offset, domain.length)
-      message.length := calldataload(message.offset)
-
-      types.offset := add(domain.offset, domain.length)
+      // offset to domain block
+      temp := add(calldataload(0x04), 0x04)
+      domain.length := calldataload(temp)
+      domain.offset := add(temp, 0x20)
+      // offset to message block
+      temp := add(calldataload(0x24), 0x04)
+      message.length := calldataload(temp)
+      message.offset := add(temp, 0x20)
+      // offset to types block
+      temp := add(calldataload(0x44), 0x04)
+      types.length := calldataload(temp)
+      types.offset := add(temp, 0x20)
     }
 
     signTypedMessage(domain, message, types);
