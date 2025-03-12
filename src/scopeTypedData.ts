caaa219a9dce6924a8243872f29e1c2a6bb3a058
Cristóvão <cris@gnosisguild.org>
2025-03-12 12:12:59 +0100

    scopeTypedData - function that scoped access to SignTypedMessageLib

--- src/scopeTypedData.ts
+++ src/scopeTypedData.ts
@@ -1,94 +1,74 @@
-import { ParamType } from "ethers";
-import { TypedData } from "viem";
-import { Condition, Operator, ParameterType, c } from "zodiac-roles-sdk";
+import { TypedData } from "abitype";
+import { AbiCoder, Interface } from "ethers";
+import { Condition, Operator, ParameterType, rolesAbi } from "zodiac-roles-sdk";
 
-/**
- * Maps over a condition structure formulated to scope the typed data object,
- * turning it into a condition structure scoping the recursively ABI encoded value as sent to the contract.
- * Once the contract can decode directly from calldata, we should be able to send the original conditions structure so this function will be obsolete.
- **/
+import { toAbiParams } from "./typed-data";
+
 export const scopeTypedData = ({
-  condition,
+  domain,
+  message,
   types,
-  type,
-  scopeSignature,
 }: {
-  condition: Condition;
+  domain: Condition;
+  message: Condition;
   types: TypedData;
-  type: string;
-  /** Controls whether the top-level struct type signature shall be scoped or not */
-  scopeSignature?: boolean;
 }): Condition => {
-  // carry through logical conditions
-  if (condition.paramType === ParameterType.None) {
-    return {
-      ...condition,
-      children: condition.children?.map((child) =>
-        scopeTypedData({ condition: child, types, type: type, scopeSignature }),
-      ),
-    };
+  if (domain.paramType !== ParameterType.AbiEncoded) {
+    throw new Error("Domain not AbiEncoded condition");
   }
 
-  // array
-  const isArray = type.includes("[");
-  if (isArray) {
-    if (condition.paramType !== ParameterType.Array) {
-      throw new Error(`Expected Array condition for type ${type}`);
-    }
-    if (
-      ![
-        Operator.ArrayEvery,
-        Operator.ArraySome,
-        Operator.ArraySubset,
-        Operator.Matches,
-      ].includes(condition.operator)
-    ) {
-      throw new Error(
-        `Only supporting ArrayEvery, ArraySome, ArraySubset, Matches operators for array types`,
-      );
-    }
-
-    const elementType = type.split("[")[0];
-    return c.abiEncodedMatches(
-      [
-        {
-          ...condition,
-          children: condition.children?.map((child) =>
-            scopeTypedData({ condition: child, types, type: elementType }),
-          ),
-        },
-      ],
-      ["bytes[]"],
-    )(ParamType.from("bytes"));
+  if (message.paramType !== ParameterType.AbiEncoded) {
+    throw new Error("Message not AbiEncoded condition");
   }
 
-  // struct
-  const isStruct = type in types;
-  if (isStruct) {
-    if (condition.paramType !== ParameterType.Tuple) {
-      throw new Error(`Expected Tuple condition for type ${type}`);
-    }
-    if (condition.operator !== Operator.Matches) {
-      throw new Error(`Only supporting Matches operator for struct types`);
-    }
-
-    const structFields = types[type];
-    return c.abiEncodedMatches(
-      [
-        structFields.map(({ type }, index) =>
-          condition.children && !!condition.children[index]
-            ? scopeTypedData({
-                condition: condition.children[index],
-                types,
-                type,
-              })
-            : undefined,
-        ),
-      ],
-      ["bytes[]"],
-    )(ParamType.from("bytes"));
+  if (!types["EIP712Domain"]) {
+    throw new Error("TypedData does not include EIP712Domain");
   }
 
-  // basic types
-  return c.abiEncodedMatches([condition], [type])(ParamType.from("bytes"));
+  return {
+    paramType: ParameterType.Calldata,
+    operator: Operator.Matches,
+    children: [domain, message, typesCondition(types)],
+  };
 };
+
+function typesCondition(types: TypedData): Condition {
+  const abiParams = toAbiParams({ types });
+  const compValue = AbiCoder.defaultAbiCoder().encode(
+    ["(uint256,bytes32,uint256[])[]"],
+    [abiParams.map((p) => [Number(p._type), p.typeHash, p.fields])],
+  );
+  return {
+    paramType: ParameterType.Array,
+    operator: Operator.EqualTo,
+    compValue: compValue as any,
+    children: [
+      {
+        paramType: ParameterType.Tuple,
+        operator: Operator.Pass,
+        children: [
+          {
+            paramType: ParameterType.Static,
+            operator: Operator.Pass,
+          },
+          {
+            paramType: ParameterType.Static,
+            operator: Operator.Pass,
+          },
+          {
+            paramType: ParameterType.Array,
+            operator: Operator.Pass,
+            children: [
+              {
+                paramType: ParameterType.Static,
+                operator: Operator.Pass,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+}
+
+export const iface = Interface.from(rolesAbi);
